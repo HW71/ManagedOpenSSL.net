aa9ba435792b467767cc4ea4c8f48d9527ac1b4d
Erik Fornoff <erik.fornoff@gmail.com>
2017-04-21 09:25:16 +0200

    Update AssemblyInfo.cs
    - Bump version to 1.0.0.0
    - changed several properties to reflect new assembly name

--- ManagedOpenSsl/Properties/AssemblyInfo.cs
+++ ManagedOpenSsl/Properties/AssemblyInfo.cs
@@ -30,12 +30,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenSSL.NET")]
+[assembly: AssemblyTitle("ManagedOpenSSL.net")]
 [assembly: AssemblyDescription("A managed wrapper for OpenSSL")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("OpenSSL.NET")]
-[assembly: AssemblyProduct("OpenSSL.NET")]
-[assembly: AssemblyCopyright("Copyright (c) OpenSSL.NET 2006-2012")]
+[assembly: AssemblyCompany("ManagedOpenSSL.net")]
+[assembly: AssemblyProduct("ManagedOpenSSL.net")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -56,5 +56,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
